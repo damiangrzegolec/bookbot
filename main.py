eb1e647105a6cc1720494b8a016630e8ebf025ea
recursionDev <contact@recursioncorp.pl>
2023-07-21 18:23:59 +0200

    count letters

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+def count_letters(path):
+    letters = {}
+
+    with open(path) as f:
+        file_cont = f.read()
+        words = file_cont.split()
+        for word in words:
+            for l in word.lower():
+                if l in letters:
+                    letters[l] += 1
+                else:
+                    letters[l] = 1
+    
+    print(letters)
+
+def print_words(path):
+    with open(path) as f:
+        file_cont = f.read()
+        words = file_cont.split()
+        print(words)
+
+
 def count_words(path):
     count = 0
     with open(path) as f:
@@ -7,5 +29,5 @@ def count_words(path):
             count += 1
     return count
 
-print(count_words("books/frank.txt"))
+print(count_letters("books/frank.txt"))
 
