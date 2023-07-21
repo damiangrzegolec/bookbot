7923a8a3f0d5e38b552de2d5e319685af2e2299b
recursionDev <contact@recursioncorp.pl>
2023-07-21 17:46:34 +0200

    count words

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("hello world")
+def count_words(path):
+    count = 0
+    with open(path) as f:
+        file_cont = f.read()
+        words = file_cont.split()
+        for _ in words:
+            count += 1
+    return count
+
+print(count_words("books/frank.txt"))
 
